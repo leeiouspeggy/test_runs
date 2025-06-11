dcd47570ed512055a131f047b1450664d3784738
Afriyie Ofori Boachie <afriyieoforiboachie@gmail.com>
2025-06-11 07:54:10 +0000

    added decisions

--- traffic_sign.py
+++ traffic_sign.py
@@ -3,6 +3,18 @@
 #and decide to pass it on the right (red) or left (green).
 
 def traffic_sign_color():
-    color = input("Traffic sign color received from sensor,red/green:")
-    return color
+    return input("Traffic sign color received from sensor,red/green: ").lower()
 #depth camera input is used
+
+def pass_direction(color):
+    if color == "red":
+        return "pass right"
+    elif color == "green":
+        return "pass left"
+    else:
+        return "color unrecognized,code error"
+    
+
+color = traffic_sign_color()
+decide = pass_direction(color)
+print("move", decide)
