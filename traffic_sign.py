bffcca19a80785e4ae913bca12aacc72b27a27e4
Afriyie Ofori Boachie <afriyieoforiboachie@gmail.com>
2025-06-11 07:27:58 +0000

    initial commit

---
+++ traffic_sign.py
@@ -0,0 +1,8 @@
+#Simulate a basic self-driving car decision for traffic signs.
+#The car should detect a traffic sign color (red/green) 
+#and decide to pass it on the right (red) or left (green).
+
+def traffic_sign_color():
+    color = input("Traffic sign color received from sensor,red/green:")
+    return color
+#depth camera input is used
